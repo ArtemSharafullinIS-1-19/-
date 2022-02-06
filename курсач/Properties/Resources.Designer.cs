74ce0bf3da0effae86ca9ebaee05d2f80b08d7df
Артём шарафуллин <rejs03@mail.ru>
2022-02-06 10:21:47 +0500

    menu.cs

--- курсач/Properties/Resources.Designer.cs
+++ курсач/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace курсач.Properties {
     // с помощью такого средства, как ResGen или Visual Studio.
     // Чтобы добавить или удалить член, измените файл .ResX и снова запустите ResGen
     // с параметром /str или перестройте свой проект VS.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -66,6 +66,16 @@ namespace курсач.Properties {
         internal static System.Drawing.Bitmap _2018_kawasaki_zx10r_4k_t8 {
             get {
                 object obj = ResourceManager.GetObject("2018-kawasaki-zx10r-4k-t8", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _8NOhnHnAbCs {
+            get {
+                object obj = ResourceManager.GetObject("8NOhnHnAbCs", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
